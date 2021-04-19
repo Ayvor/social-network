015ca04a220ccad35ccda2d2106d5ceae419e701
Ayvor <ayv.dzhu@gmail.com>
2021-04-19 05:11:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import { Route } from "react-router-dom";
-import Header from "./component/Header/Header";
+import HeaderContainer from "./component/Header/HeaderContainer";
 import Navbar from "./component/Navbar/Navbar";
-// import Profile from "./component/Profile/Profile";
-//import Dialogs from './component/Dialogs/Dialogs';
 import News from "./component/News/News";
 import Music from "./component/Music/Music";
 import Settings from "./component/Settings/Settings";
@@ -12,21 +10,21 @@ import UsersContainer from "./component/Users/UsersContainer";
 import ProfileContainer from "./component/Profile/ProfileContainer";
 
 const App = (props) => {
-    return (
-        <div className="app-wrapper">
-            <Header />
-            {/* <Navbar state={props.state.sideBar} /> */}
-            <Navbar />
-            <div className="app-wrapper-content">
-                <Route path="/profile" render={() => <ProfileContainer />} />
-                <Route path="/dialogs" render={() => <DialogsContainer />} />
-                <Route path="/news" component={News} />
-                <Route path="/music" component={Music} />
-                <Route path="/settings" component={Settings} />
-                <Route path="/users" render={() => <UsersContainer />} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="app-wrapper">
+      <HeaderContainer />
+      {/* <Navbar state={props.state.sideBar} /> */}
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+        <Route path="/dialogs" render={() => <DialogsContainer />} />
+        <Route path="/news" component={News} />
+        <Route path="/music" component={Music} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/users" render={() => <UsersContainer />} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
