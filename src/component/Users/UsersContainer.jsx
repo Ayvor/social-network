0697848a16f23e7aa95ebfdef16d51cd5ae93997
Ayvor <ayv.dzhu@gmail.com>
2021-06-14 13:26:54 +0300

    Add reselect 2

--- src/component/Users/UsersContainer.jsx
+++ src/component/Users/UsersContainer.jsx
@@ -5,8 +5,16 @@ import {
   unfollow,
   setCurrentPage,
   toggleFollowingProgress,
-  getUsers,
+  requesUsers,
 } from '../../Redux/users-reducer';
+import {
+  getUsersSuperSelector,
+  getCurrentPage,
+  getFollowingInProgress,
+  getIsFetching,
+  getPageSize,
+  getTotalUsersCount,
+} from '../../Redux/users-selectors';
 import Users from './Users';
 import Preloader from '../Common/Preloader/Preloader';
 import { withAuthRedirect } from '../hoc/withAuthRedirect';
@@ -14,11 +22,11 @@ import { compose } from 'redux';
 
 class UsersContainer extends React.Component {
   componentDidMount() {
-    this.props.getUsers(this.props.currentPage, this.props.pageSize);
+    this.props.requesUsers(this.props.currentPage, this.props.pageSize);
   }
 
   onPageChanged = (pageNumber) => {
-    this.props.getUsers(pageNumber, this.props.pageSize);
+    this.props.requesUsers(pageNumber, this.props.pageSize);
   };
 
   render() {
@@ -52,12 +60,12 @@ class UsersContainer extends React.Component {
 // };
 let mapStateToProps = (state) => {
   return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress,
+    users: getUsersSuperSelector(state),
+    pageSize: getPageSize(state),
+    totalUsersCount: getTotalUsersCount(state),
+    currentPage: getCurrentPage(state),
+    isFetching: getIsFetching(state),
+    followingInProgress: getFollowingInProgress(state),
   };
 };
 
@@ -68,6 +76,6 @@ export default compose(
     unfollow,
     setCurrentPage,
     toggleFollowingProgress,
-    getUsers,
+    getUsers: requesUsers,
   }),
 )(UsersContainer);
